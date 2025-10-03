0fb0c63c3d5f8a6e9c85052d48bb9b5d3c79c907
Mikita <nikpyzh5@gmail.com>
2025-10-03 14:27:37 +0300

    chore: include built files for 1.5.3

--- lib/specs/NativeBleNitro.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NativeBleNitro } from './NativeBleNitro.nitro';
-declare const NativeBleNitroImpl: NativeBleNitro;
-export default NativeBleNitroImpl;
-export * from './NativeBleNitro.nitro';
